6fabc36148d6cfa90f1df29cc69ca5254f819d1b
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-05-18 22:13:10 +0800

    Head First

---
+++ jQuery/chapter3/03.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#switcher-default').addClass('selected')
+	$('#switcher button').click(function(event) {
+		var bodyClass = this.id.split('-')[1];
+		$('body').removeClass().addClass(bodyClass);
+		$('#switcher button').removeClass('selected');
+		$(this).addClass('selected');
+		event.stopPropagation();
+	});
+}); 
