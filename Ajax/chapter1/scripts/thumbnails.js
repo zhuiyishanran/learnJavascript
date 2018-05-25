39d2e5c3e0ce1b7b0b917336487187f84c697430
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-05-25 15:44:44 +0800

    Ajax

---
+++ Ajax/chapter1/scripts/thumbnails.js
@@ -0,0 +1,51 @@
+window.onload = initPage;
+
+function initPage() {
+	thumbs = document.getElementById('thumbnailPane').getElementsByTagName('img');
+	for (var i = 0; i < thumbs.length; i++) {
+		image = thumbs[i];
+		image.onclick = function () {
+			detailURL = 'images/' + this.title + '-detail.jpg';
+			document.getElementById('itemDetail').src = detailURL;
+			getDetails(this.title);
+		}
+	}
+}
+
+function createRequest() {
+  try {
+    request = new XMLHttpRequest();
+  } catch (tryMS) {
+    try {
+      request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (otherMS) {
+      try {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (failed) {
+        request = null;
+      }
+    }
+  }
+  return request;
+}
+
+function getDetails(itemName) {
+  request = createRequest();
+  if (request == null) {
+    alert("Unable to create request");
+    return;
+  }
+  var url= "getDetails.php?ImageID=" + escape(itemName);
+  request.open("GET", url, true);
+  request.onreadystatechange = displayDetails;
+  request.send(null);
+}
+
+function displayDetails() {
+  if (request.readyState == 4) {
+    if (request.status == 200) {
+      detailDiv = document.getElementById('description');
+      detailDiv.innerHTML = request.responseText;
+    }
+  }
+}
