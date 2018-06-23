9adbc29e155a586b75944b7a64d5686fe1ec46a9
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-06-23 20:53:46 +0800

    react2.5

--- react/bbs/src/PostList.js
+++ react/bbs/src/PostList.js
@@ -1,24 +1,55 @@
 import React, { Component } from "react";
 import PostItem from "./PostItem";
-
-const data = [
-	{title:"javascript", author:"li", date:"2018-6-22 21:07"},
-	{title:"python", author:"li", date:"2018-6-22 21:07"},
-	{title:"java", author:"Yang", date:"2018-6-22 21:07"}
-];
-
+import "./PostList.css";
 
 class PostList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: []
+		};
+		this.timer = null;
+		this.handleVote = this.handleVote.bind(this);
+	}
+
+	componentDidMount() {
+		this.timer = setTimeout(()=>{
+			this.setState({
+				posts: [
+					{id:1, title:"大家一起来讨论React吧", author:"react", date:"2018", vote:0},
+					{id:2, title:"前端框架，你最爱哪一个", author:"angular", date:"2018", vote:0},
+					{id:3, title:"web app的时代已经到来", author:"vue", date:"2019", vote:0}
+				]
+			});
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		if(this.timer) {
+			clearTimeout(this.timer);
+		}
+	}
+
+	handleVote(id){
+		const posts = this.state.posts.map(item => {
+			const newItem = item.id === id ? {...item, vote: ++item.vote} : item;
+			return newItem;
+		});
+		this.setState({
+			posts
+		});
+	}
+
 	render() {
 		return (
-			<div>
-				帖子列表：
+			<div className="container">
+				<h2>帖子列表：</h2>
 				<ul>
-					{data.map(item => 
+					{this.state.posts.map(item => 
 						<PostItem
-							title={item.title}
-							author={item.author}
-							date={item.date}
+							key = {item.id}
+							post = {item}
+							onVote = {this.handleVote}
 						/>
 					)}
 				</ul>
