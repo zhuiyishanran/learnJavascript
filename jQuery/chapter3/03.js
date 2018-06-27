7a97becffd42567397cefad79a4e662deabdb3eb
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-06-27 09:43:59 +0800

    jquery3

--- jQuery/chapter3/03.js
+++ jQuery/chapter3/03.js
@@ -1,14 +1,20 @@
 $(function() {
+	$('#switcher').hover(function() {
+		$(this).addClass('hover');
+	}, function() {
+		$(this).removeClass('hover');
+	});
+});
+$(function() {
 	$('#switcher-default').addClass('selected');
-	$('#switcher button').on('click', function () {
-		$('body').removeClass();
-		$('#switcher button').removeClass('selected');
-		$(this).addClass('selected');
-	});
-	$('#switcher-narrow').on('click', function() {
-		$('body').removeClass().addClass('narrow');
-	});
-	$('#switcher-large').on('click', function () {
-		$('body').removeClass().addClass('large');
+	$('#switcher').click(function(event) {
+		if ($(event.target).is('button')) {
+			var bodyClass = event.target.id.split('-')[1];
+			$('body').removeClass().addClass(bodyClass);
+			$('#switcher button').removeClass('selected');
+			$(event.target).addClass('selected');
+		} else {
+			$('#switcher button').toggleClass('hidden');
+		}
 	});
 });
