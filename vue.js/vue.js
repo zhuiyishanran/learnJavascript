35226e9ee87e519a03bbec2f733fcbeb23ea46e7
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-07-12 21:49:41 +0800

    vue.js

--- vue.js/vue.js
+++ vue.js/vue.js
@@ -57,4 +57,36 @@ var app7 = new Vue({
 			{ id: 2, text: 'something else'}
 		]
 	}
-})
+});
+
+var example1 = new Vue({
+	el: '#example-1',
+	data: {
+		items: [
+			{ message: 'Foo' },
+			{ message: 'Bar' }
+		]
+	}
+});
+
+var example2 = new Vue({
+	el: '#example-2',
+	data: {
+		parentMessage: 'parent',
+		items: [
+			{ message: 'Foo' },
+			{ message: 'Bar' }
+		]
+	}
+});
+
+new Vue({
+	el: '#v-for-object',
+	data: {
+		object: {
+			firstName: 'John',
+			lastName: 'Doe',
+			age: 30
+		}
+	}
+});
